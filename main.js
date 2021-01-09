928de4f475b72ffce45fed114357e2cc056a1e6d
gino192837 <51021097+gino192837@users.noreply.github.com>
2021-01-09 08:27:08 +0200

    Change from port number to address

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ fastify.register(apiRoute, { prefix: '/api' })
 
 const start = async () => {
     try {
-        const server = await fastify.listen(4594)
+        const server = await fastify.listen('http://139.162.157.130:8855/')
         console.log(`Server running on ${server}`)
     } catch (err) {
         fastify.log.error(err)
